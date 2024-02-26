bf22cddc486a4b06514bf9188159983621e72b17
Clément Voisin <clembs@clembs.com>
2024-02-26 14:34:37 +0100

    fix: tree shaking for material icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import entryShakingPlugin from 'vite-plugin-entry-shaking';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		await entryShakingPlugin({
+			targets: ['svelte-google-materialdesign-icons'],
+			extensions: ['svelte']
+		})
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
