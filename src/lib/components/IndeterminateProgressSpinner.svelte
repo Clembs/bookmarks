19bae27cf97aec506f01a5535a1e4f9dd4305eb7
Clément Voisin <clembs@clembs.com>
2024-02-27 17:29:40 +0100

    fix: progress spinner animates properly

--- src/lib/components/IndeterminateProgressSpinner.svelte
+++ src/lib/components/IndeterminateProgressSpinner.svelte
@@ -1,58 +1,51 @@
 <!-- 
-  Remixed from n3r4zzurr0's svg-spinners (MIT License)
-  https://github.com/n3r4zzurr0/svg-spinners/blob/main/svg-css/90-ring.svg?short_path=74838c2
-  and Fran Pérez's Material Design Spinner (MIT License)
-  https://codepen.io/mrrocks/pen/ExLovj
+  Remixed from Mobbin's loading spinners made in CSS https://mobbin.com
 -->
 
-<svg
-	class="loading-spinner"
-	width="24"
-	height="24"
-	viewBox="0 0 26 26"
-	xmlns="http://www.w3.org/2000/svg"
->
-	<path
-		d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
+<script lang="ts">
+	let { size = 16 } = $props();
+</script>
+
+<svg height={size} width={size} class="loading-spinner" viewBox="0 0 24 24" stroke="currentColor">
+	<circle
 		id="spinner"
-		fill="currentColor"
+		cx="12"
+		cy="12"
+		r="10"
+		fill="none"
+		stroke-linecap="round"
+		stroke-width="3.6"
 	/>
 </svg>
 
 <style lang="scss">
 	.loading-spinner {
-		--animation-duration: 1.4s;
-		--offset: 63;
 		color: var(--color-primary);
-		animation: spin var(--animation-duration) linear infinite;
+
+		animation: spin 2s linear infinite;
 
 		#spinner {
-			transform-origin: center;
-			stroke-dasharray: var(--offset);
-			stroke-dashoffset: 0;
-			animation: dash var(--animation-duration) ease-in-out infinite;
+			animation: worm 1.4s ease-in-out infinite;
 		}
-		@keyframes spin {
-			0% {
-				transform: rotate(0deg);
-			}
-			100% {
-				transform: rotate(270deg);
-			}
-		}
+	}
 
-		@keyframes dash {
-			0% {
-				stroke-dashoffset: var(--offset);
-			}
-			50% {
-				stroke-dashoffset: calc(var(--offset) / 4);
-				transform: rotate(135deg);
-			}
-			100% {
-				stroke-dashoffset: var(--offset);
-				transform: rotate(450deg);
-			}
+	@keyframes spin {
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+	@keyframes worm {
+		0% {
+			stroke-dasharray: 1, 150;
+			stroke-dashoffset: 0;
+		}
+		50% {
+			stroke-dasharray: 90, 150;
+			stroke-dashoffset: -35;
+		}
+		100% {
+			stroke-dasharray: 90, 150;
+			stroke-dashoffset: -62;
 		}
 	}
 </style>
