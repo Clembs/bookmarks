aca432e7e0a8e3ca4ca6a82f8fcc15268e62f236
Clément Voisin <clembs@clembs.com>
2024-03-20 15:31:05 +0100

    feat: scrim component

---
+++ src/lib/components/Scrim.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	let { onclose } = $props<{
+		onclose: () => void;
+	}>();
+</script>
+
+<div
+	aria-hidden="true"
+	onclick={onclose}
+	onkeydown={(ev) => {
+		console.log(ev.key);
+		if (ev.key === 'Escape') {
+			onclose();
+		}
+	}}
+	class="scrim"
+/>
+
+<style lang="scss">
+	.scrim {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: var(--neutral-0);
+		opacity: 0.32;
+		z-index: 3;
+	}
+</style>
