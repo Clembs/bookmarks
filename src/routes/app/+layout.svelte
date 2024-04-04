36e4bce18aaafbd57562d9c07ae2fdd4561886c1
Clément Voisin <clembs@clembs.com>
2024-04-04 18:08:34 +0200

    refactor: sidebar to its own component

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -1,190 +1,68 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import Button from '$lib/components/Button.svelte';
-	import { Menu, Settings, Menu_open } from 'svelte-google-materialdesign-icons';
+	import Dialog from '$lib/components/Dialog.svelte';
+	import TextInput from '$lib/components/TextInput.svelte';
+	import { Menu, Menu_open, Add } from 'svelte-google-materialdesign-icons';
+	import Sidebar from './Sidebar.svelte';
 
 	let { children, data } = $props();
 
 	let isSidebarOpen = $state(false);
-
-	$effect(() => {
-		const isExpanded = window.matchMedia('(min-width: 840px)').matches;
-
-		if (isExpanded) {
-			isSidebarOpen = true;
-		}
-	});
 </script>
 
-<svelte:window
-	onresize={(ev) => {
-		const isExpanded = window.matchMedia('(min-width: 840px)').matches;
-
-		isSidebarOpen = isExpanded;
-	}}
-/>
-
-{#snippet toggleButton(type: "close" | 'open')}
-	<Button style="text" icon onclick={() => (isSidebarOpen = type === 'open')}>
-		{#if type === 'close'}
-			<Menu_open />
-		{:else}
-			<Menu />
-		{/if}
-	</Button>
-{/snippet}
-
 <div id="layout">
-	<div id="menu-btn">
-		{@render toggleButton('open')}
-	</div>
-
-	<div id="sidebar-wrapper" data-active={isSidebarOpen}>
-		<aside>
-			<div id="title">
-				Bookmarks
-				{@render toggleButton('close')}
-			</div>
-
-			<ul id="top-items">
-				<a data-active={$page.url.pathname === '/app'} href="/app">All bookmarks</a>
-				{#each data.categories as category (category.id)}
-					<a data-active={$page.url.pathname === `/app/${category.id}`} href="/app/{category.id}">
-						{category.name}
-					</a>
-				{/each}
-			</ul>
-
-			<ul id="bottom-items">
-				<a href="/settings">
-					<Settings size="24" />
-					Settings
-				</a>
-			</ul>
-		</aside>
-	</div>
+	<Sidebar bind:isSidebarOpen />
 
 	<div id="scrollable-wrapper">
+			<div id="menu-btn">
+				<Button
+					style="text"
+					icon
+					onclick={() => {
+						isSidebarOpen = !isSidebarOpen;
+					}}
+				>
+				{#if isSidebarOpen}
+					<Menu_open />
+				{:else}
+					<Menu />
+				{/if}
+				</Button>
+			</div>
 		<main>
+
 			{@render children()}
 		</main>
 	</div>
 </div>
 
 <style lang="scss">
-	@import '../../styles/vars.scss';
-
 	#layout {
 		display: flex;
 		height: 100vh;
+		background-color: var(--color-surface-container-low);
+		padding-left: var(--space-2);
 
 		#menu-btn {
-			position: fixed;
+			position: absolute;
 			top: var(--space-4);
 			left: var(--space-4);
-			z-index: 0;
-		}
-
-		#sidebar-wrapper {
-			min-width: 360px;
-			overflow: hidden;
-			width: 0;
-			background-color: var(--color-surface-container-low);
-			color: var(--color-on-surface-variant);
-			border-radius: 0 var(--round-lg) var(--round-lg) 0;
-			transition: min-width var(--transition-enter-screen-emphasized);
-			will-change: min-width;
 			z-index: 1;
-
-			:global(svg) {
-				flex-shrink: 0;
-			}
-
-			&[data-active='false'] {
-				min-width: 0;
-				padding: 0;
-				transition: min-width var(--transition-exit-screen-emphasized);
-				will-change: min-width;
-
-				aside {
-					translate: -24px;
-					opacity: 0;
-					transition:
-						translate var(--transition-exit-screen-emphasized),
-						opacity var(--transition-exit-screen-emphasized);
-					will-change: translate, opacity;
-				}
-			}
-		}
-
-		aside {
-			display: flex;
-			flex-direction: column;
-			height: 100%;
-			overflow-x: hidden;
-			// overflow-y: scroll;
-			z-index: 9;
-			padding: var(--space-3);
-			transition:
-				translate var(--transition-enter-screen-emphasized),
-				opacity var(--transition-enter-screen-emphasized);
-			will-change: translate, opacity;
-
-			#title {
-				font-size: 1.5rem;
-				font-family: var(--fonts-headings);
-				font-weight: 600;
-				padding: var(--space-3) 0;
-				padding-left: var(--space-2);
-				display: flex;
-				justify-content: space-between;
-				align-items: center;
-			}
-
-			ul {
-				display: flex;
-				flex-direction: column;
-				gap: var(--space-1);
-
-				a {
-					display: flex;
-					align-items: center;
-					width: 100%;
-					overflow: hidden;
-					text-overflow: ellipsis;
-					text-wrap: nowrap;
-					white-space: nowrap;
-					height: var(--space-9);
-					gap: var(--space-4);
-					padding: var(--space-2) var(--space-4);
-					border-radius: var(--round-full);
-					color: var(--color-on-surface-variant);
-					text-decoration: none;
-
-					&:hover {
-						background-color: var(--color-surface-container);
-					}
-
-					&[data-active] {
-						background-color: var(--color-secondary-container);
-						color: var(--color-on-secondary-container);
-						font-weight: 500;
-					}
-				}
-
-				&#bottom-items {
-					margin-top: auto;
-				}
-			}
 		}
 
 		#scrollable-wrapper {
+			position: relative;
 			flex: 1;
 			overflow-y: auto;
+			background-color: var(--color-surface-container-lowest);
+			border-radius: var(--round-xlg);
+			margin: var(--space-2);
+			margin-left: 0;
 
 			main {
 				position: relative;
-				padding: var(--space-4);
+				padding: var(--space-2);
 				max-width: 700px;
 				margin: 0 auto;
 			}
---
+++ src/routes/app/Sidebar.svelte
@@ -0,0 +1,164 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { Settings, Add } from 'svelte-google-materialdesign-icons';
+
+	let { isSidebarOpen = $bindable(false) } = $props();
+	let isCreationModalOpen = $state(false);
+
+	$effect(() => {
+		const isExpanded = window.matchMedia('(min-width: 1200px)').matches;
+
+		isSidebarOpen = isExpanded;
+	});
+</script>
+
+{#if isCreationModalOpen}
+	<Dialog
+		showModal
+		headline="Create category"
+		actions={[
+			{
+				label: 'Cancel',
+				action: () => (isCreationModalOpen = false)
+			},
+			{
+				label: 'Create',
+				action: 'submit'
+			}
+		]}
+		onclose={() => (isCreationModalOpen = false)}
+	>
+		<form method="post" action="/api/category?">
+			<TextInput name="name" label="Name" required autofocus />
+		</form>
+	</Dialog>
+{/if}
+
+<svelte:window
+	onresize={(ev) => {
+		const isExpanded = window.matchMedia('(min-width: 1200px)').matches;
+
+		isSidebarOpen = isExpanded;
+	}}
+/>
+
+<div id="sidebar-wrapper" data-active={isSidebarOpen}>
+	<aside>
+		<div id="title">Bookmarks</div>
+
+		<ul id="top-items">
+			<a class="item" data-active={$page.url.pathname === '/app'} href="/app">All bookmarks</a>
+			{#each $page.data.categories as category (category.id)}
+				<a
+					class="item"
+					data-active={$page.url.pathname === `/app/${category.id}`}
+					href="/app/{category.id}"
+				>
+					{category.name}
+				</a>
+			{/each}
+			<button class="item" onclick={() => (isCreationModalOpen = true)}>
+				<Add size="24" />
+				Create category
+			</button>
+		</ul>
+
+		<ul id="bottom-items">
+			<a class="item" href="/settings">
+				<Settings size="24" />
+				Settings
+			</a>
+		</ul>
+	</aside>
+</div>
+
+<style lang="scss">
+	@import '../../styles/vars.scss';
+
+	#sidebar-wrapper {
+		min-width: 360px;
+		overflow: hidden;
+		width: 0;
+		background-color: var(--color-surface-container-low);
+		color: var(--color-on-surface-variant);
+		border-radius: 0 var(--round-lg) var(--round-lg) 0;
+
+		// transition: min-width 200ms linear;
+
+		transition: min-width var(--transition-enter-screen-emphasized);
+		// will-change: min-width;
+		z-index: 1;
+
+		:global(svg) {
+			flex-shrink: 0;
+		}
+
+		&[data-active='false'] {
+			min-width: 0;
+			padding: 0;
+			transition: min-width var(--transition-exit-screen-emphasized);
+			// will-change: min-width;
+		}
+	}
+
+	aside {
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		width: 360px;
+		overflow-x: hidden;
+		// overflow-y: scroll;
+		z-index: 9;
+		padding: var(--space-3);
+		// transition:
+		// 	translate var(--transition-enter-screen-emphasized),
+		// 	opacity var(--transition-enter-screen-emphasized);
+		// will-change: translate, opacity;
+
+		#title {
+			font-size: 1.5rem;
+			font-family: var(--fonts-headings);
+			font-weight: 600;
+			padding: var(--space-3) 0;
+			padding-left: var(--space-2);
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+		}
+
+		ul {
+			display: flex;
+			flex-direction: column;
+			gap: var(--space-1);
+
+			.item {
+				display: flex;
+				align-items: center;
+				width: 100%;
+				overflow: hidden;
+				text-overflow: ellipsis;
+				text-wrap: nowrap;
+				white-space: nowrap;
+				gap: var(--space-4);
+				padding: var(--space-3) var(--space-4);
+				border-radius: var(--round-md);
+				color: var(--color-on-surface-variant);
+				text-decoration: none;
+
+				&:hover {
+					background-color: var(--color-surface-container);
+				}
+
+				&[data-active] {
+					background-color: var(--color-surface-container-highest);
+					color: var(--color-on-surface);
+					font-weight: 500;
+				}
+			}
+
+			&#bottom-items {
+				margin-top: auto;
+			}
+		}
+	}
+</style>
