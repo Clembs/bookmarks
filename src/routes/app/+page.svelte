fb29ec598e0520a381006cb5ca56a19ca2771be8
Clément Voisin <clembs@clembs.com>
2024-04-17 22:35:23 +0200

    refactor: new bookmark input, moved from main page

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,85 +1,17 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
-	import TextInput from '$lib/components/TextInput.svelte';
-	import { Bookmark } from '$lib/helpers/bookmark.svelte';
+	import { Bookmarks } from '$lib/helpers/bookmark.svelte';
 	import BookmarkList from '$lib/components/BookmarkList.svelte';
-
-	let value = $state('');
-	let error = $state('');
-	let textInput = $state<HTMLInputElement | HTMLTextAreaElement | undefined>();
-	let form = $state<HTMLFormElement | undefined>();
+	import BookmarkInput from './BookmarkInput.svelte';
 
 	const { data } = $props();
 
-	let bookmarks = $derived(data.bookmarks.map((b) => new Bookmark(b)));
+	let bookmarks = $state(new Bookmarks(data.bookmarks));
 </script>
 
-<svelte:window
-	onkeydown={(ev) => {
-		const isNothingFocused = !document.activeElement?.matches('input, textarea');
-		// if it's a letter or number and nothing is focused, focus the text input
-		if (
-			!ev.ctrlKey &&
-			!ev.metaKey &&
-			!ev.altKey &&
-			!ev.shiftKey &&
-			ev.key.length === 1 &&
-			isNothingFocused
-		) {
-			textInput?.focus();
-		}
-		if ((ev.ctrlKey || ev.metaKey) && ev.key === 'f') {
-			ev.preventDefault();
-			textInput?.focus();
-		}
-		if ((ev.ctrlKey || ev.metaKey) && ev.key === 'v' && textInput && form && isNothingFocused) {
-			textInput.focus();
-			form.requestSubmit();
-		}
-	}}
-/>
-
 <header>
-	<h1>All bookmarks</h1>
+	<h1>All saves</h1>
 
-	<form
-		id="add-bookmark"
-		method="post"
-		bind:this={form}
-		use:enhance={() => {
-			console.log("h");
-			// optimistic update
-			data.bookmarks.push({
-				title: value,
-				value,
-				partial: true,
-			});
-	
-			return async ({ result, update }) => {
-				// data.bookmarks.pop();
-	
-				if (result.type === "failure" && result.data?.message) {
-					error = result.data.message as string;
-				}
-				
-				// if (result.type === "success" && result.data?.bookmark) {
-					// data.bookmarks.push(
-					// 	result.data.bookmark as RawBookmark
-					// );
-				// }
-				return await update();
-			}
-		}}
-	>
-		<!-- bind:error -->
-		<TextInput
-			type="text"
-			bind:value
-			bind:el={textInput}
-			name="raw"
-			placeholder="Add a URL, a color, code, or whatever else..."
-		/>
-	</form>
+	<BookmarkInput {bookmarks} />
 </header>
 
 <BookmarkList {bookmarks} />
---
+++ src/routes/app/BookmarkInput.svelte
@@ -0,0 +1,104 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { RawBookmark } from '$lib/db/types';
+	import { Bookmarks } from '$lib/helpers/bookmark.svelte';
+	import { Add } from 'svelte-google-materialdesign-icons';
+
+	let error = $state('');
+	let textInput = $state<HTMLInputElement | undefined>();
+	let value = $state('');
+	let form = $state<HTMLFormElement | undefined>();
+
+	let { bookmarks = $bindable() }: { bookmarks: Bookmarks } = $props();
+</script>
+
+<svelte:window
+	onkeydown={(ev) => {
+		const isNothingFocused = !document.activeElement?.matches('input, textarea');
+		const isMetaPressed = ev.metaKey || ev.ctrlKey;
+		// if it's a letter or number and nothing is focused, focus the text input
+		if (!isMetaPressed && !ev.altKey && !ev.shiftKey && ev.key.length === 1 && isNothingFocused) {
+			textInput?.focus();
+		}
+		if (isMetaPressed && ev.key === 'f') {
+			ev.preventDefault();
+			textInput?.focus();
+		}
+		if (isMetaPressed && ev.key === 'v' && textInput && form && isNothingFocused) {
+			textInput.focus();
+			form.requestSubmit();
+		}
+	}}
+/>
+
+<form
+	id="add-bookmark"
+	method="post"
+	action="/api/bookmarks/create/?"
+	bind:this={form}
+	use:enhance={() => {
+		bookmarks.push({
+		  title: value,
+		  value,
+		  partial: true,
+		});
+
+		return async ({ result, update }) => {
+			bookmarks.pop();
+
+			if (result.type === 'failure' && typeof result.data?.message === 'string') {
+				error = result.data.message;
+			}
+
+			if (result.type === "success" && result.data?.bookmark) {
+        bookmarks.push(
+          result.data.bookmark as RawBookmark
+			);
+			}
+			return await update();
+		};
+	}}
+>
+	<input
+		bind:this={textInput}
+		bind:value
+		type="text"
+		name="raw"
+		placeholder="Search a save or add to your saves"
+	/>
+	<button type="submit">
+		<Add />
+	</button>
+</form>
+
+<style lang="scss">
+	form {
+		display: flex;
+		border: 1px solid var(--color-outline);
+		background-color: var(--color-surface);
+		border-radius: var(--round-full);
+		margin: var(--space-4) 0;
+
+		input {
+			flex: 1;
+			padding: var(--space-4);
+			border: none;
+			outline: none;
+			border-radius: var(--round-full) 0 0 var(--round-full);
+			background-color: transparent;
+			font: inherit;
+		}
+
+		button {
+			display: grid;
+			place-items: center;
+			width: var(--space-10);
+			height: var(--space-10);
+			border: none;
+			background-color: var(--color-primary);
+			color: var(--color-on-primary);
+			border-radius: var(--round-full);
+			margin: var(--space-2);
+		}
+	}
+</style>
