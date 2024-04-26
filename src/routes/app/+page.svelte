00ef40a622857042506fa41682062126bd43062e
Clément Voisin <clembs@clembs.com>
2024-04-26 17:29:57 +0200

    fix: adapt code to new bookmarks structure

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
-	import { Bookmarks } from '$lib/helpers/bookmark.svelte';
 	import BookmarkList from '$lib/components/BookmarkList.svelte';
+	import { bookmarks } from '$lib/helpers/bookmark.svelte';
 	import BookmarkInput from './BookmarkInput.svelte';
 
 	const { data } = $props();
 
 	let value = $state('');
 
-	let bookmarks = $derived(new Bookmarks(data.bookmarks));
+	bookmarks.setBookmarks(data.bookmarks);
 </script>
 
 <header>
 	<h1>All saves</h1>
 
-	<BookmarkInput {bookmarks} bind:value />
+	<BookmarkInput bind:value />
 </header>
 
 <BookmarkList
-	bookmarks={value.length >= 2 ? bookmarks.filter((bm) => bm.raw.title?.toLowerCase()?.includes(value.toLowerCase())!) : bookmarks}
+	bookmarks={value.length >= 2 ? bookmarks.bookmarks.filter((bm) => bm.raw.title?.toLowerCase()?.includes(value.toLowerCase())!) : bookmarks.bookmarks}
 />
 
 <style lang="scss">
